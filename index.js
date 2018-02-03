367c516e2481fb3c7da3481820b4a8cbf5c6abd6
Aaroh Mankad <aarohmankad@gmail.com>
2018-02-03 13:53:44 -0800

    res => response

--- index.js
+++ index.js
@@ -24,9 +24,9 @@ let
 
 app.use(bodyParser.json());
 app.use((request, response, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    response.header('Access-Control-Allow-Origin', '*');
+    response.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    response.header('Access-Control-Allow-Headers', 'Content-Type');
 
     next();
 });
